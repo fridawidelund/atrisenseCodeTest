48c6afec46af4a4a307278189d742d79c770399c
Frida Widelund <frida.widelund@predicare.se>
2025-12-11 22:37:06 +0100

    Refactored to use numpy arrays instead of list
    Time decreased from 25ms to 0.7ms

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
-import struct
-import math
+import time
+import numpy as np
 import matplotlib.pyplot as plt
 from foxglove_streamer import stream_points_to_foxglove
 
-def to_cartesian(x_angle_deg, y_angle_deg, distance_m):
-    az = math.radians(x_angle_deg)
-    el = math.radians(y_angle_deg)
+def parse_and_convert_binary_data():
+    with open("./atrisense.bin", "rb") as f:
+        data = f.read()
 
-    x = distance_m * math.cos(el) * math.cos(az)
-    y = distance_m * math.cos(el) * math.sin(az)
-    z = distance_m * math.sin(el)
+    dt = np.dtype([('scan_number', '<u4'), ('x_angle_deg', '<f4'), ('y_angle_deg', '<f4'), 
+                ('distance', '<f4'), ('intensity', '<u2')])
+    raw_records = np.frombuffer(data, dtype=dt)
 
-    return x, y, z
+    # Vectorized cartesian conversion
+    x_angles_rad = np.radians(raw_records['x_angle_deg'])
+    y_angles_rad = np.radians(raw_records['y_angle_deg'])
+    distances = raw_records['distance']
 
-def parse_and_convert_binary_data(file_path):
-    record_format="<IfffH"
-    record_size = struct.calcsize(record_format)
-    records = []
+    x = distances * np.cos(y_angles_rad) * np.cos(x_angles_rad)
+    y = distances * np.cos(y_angles_rad) * np.sin(x_angles_rad)
+    z = distances * np.sin(y_angles_rad)
 
-    with open(file_path, "rb") as f:
-        while chunk := f.read(record_size):
-            if len(chunk) != record_size:
-                print("Incomplete record found, stopping read.")
-                break
-            scan_number, x_angle, y_angle, distance, intensity = struct.unpack(record_format, chunk)
-            x, y, z = to_cartesian(x_angle, y_angle, distance)
-            records.append((scan_number, x_angle, y_angle, distance, intensity, x, y, z))
-
-    return records
+    points = np.stack([x, y, z], axis=1)
+    return points
 
 def plot_3d_points(points, num_points):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    xs = [p[0] for p in points[0:num_points]]
-    ys = [p[1] for p in points[0:num_points]]
-    zs = [p[2] for p in points[0:num_points]]
-
+    xs, ys, zs = points[:num_points].T
     ax.scatter(xs, ys, zs)
     plt.show()
 
 def main():
-    records = parse_and_convert_binary_data("./atrisense.bin") 
-    points = [(r[-3:]) for r in records]
-    plot_3d_points(points, -1)
+    start = time.perf_counter()
+    points = parse_and_convert_binary_data()    
+    end = time.perf_counter()
+    print(f"Execution time: {(end - start)*1000:.3f} ms.")
+    print(f"Parsed {len(points)} points.")
+    print(points.shape)
     stream_points_to_foxglove(points)
+    plot_3d_points(points, -1)
 
 if __name__ == "__main__":
     main()
